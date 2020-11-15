7b2f196fa367780df23a8a58baa1bf7f99ee5e81
techtwins <gshevaughn@gmail.com>
2020-11-15 15:19:18 -0500

    done

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 62
+let num2 = 1
+
+let multiply = num1 * num2
+multiply;
+
+let random = Math.random() * 20
+
+let mod = 40 % 6
+
+let max = Math.max(12, 20, 9, 10)
+
